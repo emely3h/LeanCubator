43fab66c8b159840e5fa4ad78e58772b4e131048
Janis Peters <janis-pe@gmx.de>
2021-11-21 03:55:44 +0100

    Created Create Project button

--- lib/Pages/project_overview.dart
+++ lib/Pages/project_overview.dart
@@ -3,6 +3,8 @@ import 'dart:core';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:getwidget/components/dropdown/gf_multiselect.dart';
+import 'package:lean_cubator/Models/custom_user.dart';
 import 'package:lean_cubator/Models/project.dart';
 
 void main() {
@@ -19,7 +21,15 @@ class OV extends StatelessWidget {
 class ProjectOverview extends StatefulWidget {
   @override
   ProjectOverviewState createState() =>
-      ProjectOverviewState(projects: getProjects());
+      ProjectOverviewState(projects: getProjects(), users: getUsers());
+
+  static List<CustomUser> getUsers() {
+    List<CustomUser> users = [];
+    users.add(CustomUser(email: "email1", name: "name1", team: "team1"));
+    users.add(CustomUser(email: "email2", name: "name2", team: "team2"));
+    users.add(CustomUser(email: "email3", name: "name3", team: "team3"));
+    return users;
+  }
 
   static List<Project> getProjects() {
     List<Project> projects = [];
@@ -73,13 +83,27 @@ class ProjectOverview extends StatefulWidget {
 
 class ProjectOverviewState extends State<ProjectOverview> {
   TextEditingController editingController = TextEditingController();
+  TextEditingController _textFieldController2 = TextEditingController();
+    TextEditingController _textFieldController = TextEditingController();
   List<Project> projects = [];
   List<Project> viewable = [];
 
-  ProjectOverviewState({required this.projects}) {
+  List<CustomUser> users = [];
+
+  ProjectOverviewState({required this.projects, required this.users}) {
     viewable = projects;
   }
 
+  void addProject(Project project) {
+      projects.add(project);
+      viewable = projects
+                      .where((element) => element.name
+                          .toLowerCase()
+                          .contains(currentFilter.toLowerCase()))
+                      .toList();
+  }
+
+  late String currentFilter = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,6 +113,7 @@ class ProjectOverviewState extends State<ProjectOverview> {
             child: TextField(
               onChanged: (value) {
                 setState(() {
+                  currentFilter = value;
                   viewable = projects
                       .where((element) => element.name
                           .toLowerCase()
@@ -110,8 +135,85 @@ class ProjectOverviewState extends State<ProjectOverview> {
                   itemCount: viewable.length,
                   itemBuilder: (BuildContext context, int index) {
                     return createListTile(viewable[index]);
-                  }))
+                  })),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: () {
+                  _displayTextInputDialog(context);
+                },
+              ),
+            ],
+          ),
         ]));
+  }
+
+  late String name;
+  late List<String> members = [];
+  late String info;
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Create Project'),
+            content: Container(
+                child: Column(children: [
+              TextField(
+                onChanged: (value) {
+                  name = value;
+                },
+                controller: _textFieldController2,
+                decoration: InputDecoration(hintText: "Name"),
+              ),              
+              TextField(
+                onChanged: (value) {
+                  info = value;
+                },
+                controller: _textFieldController,
+                decoration: InputDecoration(hintText: "info"),
+              ),
+              // DropdownButton<String>(
+
+              //     items:
+              //         users.map<DropdownMenuItem<String>>((CustomUser value) {
+              //   return DropdownMenuItem<String>(
+              //     value: value.name,
+              //     child: Text(value.name),
+              //   );
+              // }).toList(),
+              // onChanged: (val) => setState(() {
+              //         print("searchSource:" + val!);
+              //         members = val!;
+              //       }))
+              GFMultiSelect(items: users.map<String>((CustomUser value) {
+                 return value.name;
+               }).toList(), onSelect: (value) { members = value.cast<int>().map<String>((e) => users[e].name).toList(); },),
+            ])),
+            actions: <Widget>[
+              ElevatedButton(
+                child: Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              ElevatedButton(
+                child: Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    addProject(Project(name: name, info: info, members: members, currentStage: "", active: true));
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
   }
 
   Widget createListTile(Project project) {
